db6f0458af80bb8efbd40f58b0fad551e9b8ef51
kyungbin <asd0096@naver.com>
2022-04-10 20:32:07 +0900

    class dfa modifed

--- lexical_analysis.cpp
+++ lexical_analysis.cpp
@@ -1,7 +1,75 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <set>
+#include <map>
+
 using namespace std;
+
+class dfa{
+private:
+    unsigned int finalState = -1;
+    unsigned int curState = 0;
+    unsigned int startState = 0;
+    bool flag = false;
+    vector<char> leximes;
+    set<unsigned int> states;
+    map<pair<unsigned int, char>, unsigned int> transitionMap;
+
+public:
+    void addState(unsigned int stateNumber){
+        states.insert(stateNumber);
+    }
+
+    void addTransition(unsigned int inputState, char ch, unsigned int outputState){
+        transitionMap.insert(make_pair(inputState, ch), outputState);
+    }
+
+    void transition(unsigned int inputState, char ch){
+        if(flag) return;
+        else{
+            
+            
+            
+            //trainsition에 대한 내용=
+        }
+    }
+
+
+    unsigned int curState(){
+        return curState;
+    }
+
+    bool acceptDfa(){
+        return flag;
+    }
+
+    void reset(){
+        curState = 0;
+    }
+
+    pair<string, string> makeLeximes(){
+        //override dfa에 따라 string 내용이 다름
+    }
+
+    bool checkDfa(char ch){
+        transition(curState, ch);
+        return acceptDfa;
+    }
+    
+}
+
+
+struct Dfa{
+    //각 dfa 모음
+};
+
+
+
+
+
+
 
 
 int main(int argc, char* argv[]) {    
